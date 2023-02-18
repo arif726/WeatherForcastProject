628f7dc6e1ca7dd7ac7d7f2b22c04fd4507e688e
arif726 <akonarif3@gmail.com>
2023-02-18 11:25:40 +0600

    js time added

--- weather.js
+++ weather.js
@@ -1,9 +1,13 @@
 const timeDateContainer = document.getElementById('time-date-container');
 const time = document.getElementById('time');
-const date = document.getElementById('date');
+const dateContainer = document.getElementById('date');
 
 
-const DAYS=[]
+const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'september', 'October', 'November', 'December'];
+
+const API_KEY = 'b1d58992db58680fd6cccfc5c33c993c'; 
+
 const displayTime = () => {
     const dateTime = new Date();
     const hrs = dateTime.getHours();
@@ -14,7 +18,18 @@ const displayTime = () => {
     const month = dateTime.getMonth();
     const day = dateTime.getDay();
 
-    time.innerHTML = hrsIn12hrFormate + ':' + min +`<span id="am-pm">${amPm}</span>` ;
+    time.innerHTML = hrsIn12hrFormate + ':' + min + `<span id="am-pm">${amPm}</span>`;
+    dateContainer.innerHTML = 
+    `
+        <span>${DAYS[day]},</span>
+        <span>${date} ${MONTHS[month]}</span>
+    `
     
 }
 setInterval(displayTime, 10);
+const getWeatherData = () => {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success);
+    })
+}
+getWeatherData();
