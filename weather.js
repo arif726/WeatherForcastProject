540ea885fae3ec1c1a161ef1b3f9d2ed87902adf
arif726 <akonarif3@gmail.com>
2023-02-18 13:06:45 +0600

    current info updated

--- weather.js
+++ weather.js
@@ -1,12 +1,13 @@
 const timeDateContainer = document.getElementById('time-date-container');
 const time = document.getElementById('time');
 const dateContainer = document.getElementById('date');
+const currentWeatherItem = document.getElementById('current-weather-items');
 
 
 const DAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'september', 'October', 'November', 'December'];
 
-const API_KEY = 'b1d58992db58680fd6cccfc5c33c993c'; 
+const API_KEY = 'b1d58992db58680fd6cccfc5c33c993c';
 
 const displayTime = () => {
     const dateTime = new Date();
@@ -18,18 +19,55 @@ const displayTime = () => {
     const month = dateTime.getMonth();
     const day = dateTime.getDay();
 
-    time.innerHTML = hrsIn12hrFormate + ':' + min + `<span id="am-pm">${amPm}</span>`;
+    time.innerHTML = hrsIn12hrFormate + ':' + min +`<span id="am-pm">${amPm}</span>`;
+
     dateContainer.innerHTML = 
     `
-        <span>${DAYS[day]},</span>
-        <span>${date} ${MONTHS[month]}</span>
-    `
+    <span>${DAYS[day]},</span>
+    <span>${date} ${MONTHS[month]}</span>
     
+    `
 }
 setInterval(displayTime, 10);
-const getWeatherData = () => {
-    navigator.geolocation.getCurrentPosition((success) => {
-        console.log(success);
+
+const getLocation =()=>{
+navigator.geolocation.getCurrentPosition((success)=>{
+    let {latitude, longitude}=success.coords;
+
+    const url =`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>{
+    showWeatherData(data);
+        
     })
+})
+};
+getLocation();
+const showWeatherData=(data)=>{
+    console.log(data);
+    currentWeatherItem.innerHTML = `
+    <div class="weather-items">
+              <span>Humidity</span>
+              <span>${data.main.humidity
+              }</span>
+            </div>
+            <div class="weather-items">
+              <span>Pressure</span>
+              <span>${data.main.pressure}</span>
+            </div>
+            <div class="weather-items">
+              <span>Wind speed</span>
+              <span>${data.wind.speed}</span>
+            </div>
+            <div class="weather-items">
+              <span>Sunrise</span>
+              <span>${data.sys.sunrise}</span>
+            </div>
+            <div class="weather-items">
+              <span>Sunset</span>
+              <span>${data.sys.sunset}</span>
+            </div>
+    `  
 }
-getWeatherData();
